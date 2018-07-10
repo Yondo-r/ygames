f13b5ee7f25f831e2012f5e3d650914e3f3668e4
yondo-r <yondo.ricardo@gmail.com>
2018-07-10 15:38:18 -0300

    estrutura inicial

---
+++ columns/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <columns msg="Este será o jogo Columns"/>
+  </div>
+</template>
+
+<script>
+import Columns from './components/Columns.vue'
+import tetris from './components/tetris.js'
+
+export default {
+  name: 'app',
+  components: {
+    Columns,
+    tetris
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ columns/src/components/Columns.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="oi()">clique</button>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+// import tetris from './tetris'
+export default {
+  name: 'Columns',
+  props: {
+    msg: String
+  },
+  methods: {
+    oi(){
+      // console.log("oi, eu sou o goku!")
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
